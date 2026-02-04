1409d77d38b256e222877bbaf85dac19f7f3c5d4
xRangerDE <xRangerDE@24-music.de>
2026-02-04 14:35:23 +0100

    Bump version to 3.6.3 for auto-update testing

--- typescript-version/src/main.ts
+++ typescript-version/src/main.ts
@@ -8,7 +8,7 @@ import { autoUpdater } from 'electron-updater';
 // ==========================================
 // CONFIG & CONSTANTS
 // ==========================================
-const APP_VERSION = '3.6.2';
+const APP_VERSION = '3.6.3';
 const UPDATE_CHECK_URL = 'http://24-music.de/version.json';
 
 // Paths
